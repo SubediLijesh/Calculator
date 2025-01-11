18119c19d41484edb012ebb75dfb3565b57c9d7f
Lijesh Subedi <subedilijesh@gmail.com>
2025-01-11 12:38:06 +0545

    version 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.calculator}>
+          <Display />
+          <ButtonsContainer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
